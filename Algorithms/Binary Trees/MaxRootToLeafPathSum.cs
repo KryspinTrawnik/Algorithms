81d44ddc604362c9b766ba4fb2bda411796bfd82
KryspinTrawnik <ktrawnik@gmail.com>
2023-05-09 06:44:21 +0100

    MaxRootToLeafPathSum

---
+++ Algorithms/Binary Trees/MaxRootToLeafPathSum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Binary_Trees
+{
+    public class MaxRootToLeafPathSum
+    {
+        public int MaxRootToLeafPathSumByRecusion(Node node) 
+        {
+            if (node == null) return int.MinValue;
+            if(node.Left  == null && node.Right == null) return (int)node.Value;
+
+
+            return (int)node.Value + Math.Max(MaxRootToLeafPathSumByRecusion(node.Left), MaxRootToLeafPathSumByRecusion(node.Right));
+        }
+    }
+}
