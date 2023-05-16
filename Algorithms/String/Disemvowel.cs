9c787e5d93eec22678d41d2a224f7c17a0bd7569
KryspinTrawnik <ktrawnik@gmail.com>
2023-05-16 19:05:57 +0100

    RemoveVowels

---
+++ Algorithms/String/Disemvowel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.String
+{
+    public class Disemvowel
+    {
+        public string RemoveVowelFromText(string str) 
+        {
+                if (string.IsNullOrEmpty(str))
+                    return "";
+                List<string> vowels = new() { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+                vowels.ForEach(x => { str = str.Replace(x, ""); });
+
+                return str;
+        }
+    }
+}
