2013ceb95bb5fe7a34bbb3cc806bbdb412b238ae
KryspinTrawnik <ktrawnik@gmail.com>
2023-05-20 07:22:51 +0100

    DotCalculator

---
+++ Algorithms/String/ConsecutiveVowels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.String
+{
+    public class ConsecutiveVowels
+    {
+        public static int GetTheVowels(string word)
+        {
+            if (string.IsNullOrEmpty(word)) 
+                return 0;
+            int count = 0;
+            char[] vowel = ['a', 'e', 'i', 'o', 'u'];
+            Queue<char> queue = new Queue<char>();
+
+            return count;
+        }
+    }
+}
---
+++ Algorithms/String/DotCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.String
+{
+    public class DotCalculator
+    {
+        public static string Calculator(string txt)
+        {
+            int cOne = 0, cTwo= 0, l = txt.Length;
+            char calc;
+
+            for (int i = 0; i < l; i++) { }
+
+        }
+    }
+}
